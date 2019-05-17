0abae7f7116cb926cbf5215d90a20eeea5f8542e
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2019-05-17 16:11:33 +0100

    Initializing data with Spring Framework Events

--- src/main/java/thegreychain/springframework/spring5webapp/model/Book.java
+++ src/main/java/thegreychain/springframework/spring5webapp/model/Book.java
@@ -13,24 +13,33 @@ public class Book {
     private  long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher = new Publisher();
 
     @ManyToMany
     @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
-    public Book(String title, String isbn, String publisher) {
-        this.title = title;
-        this.isbn = isbn;
-        this.publisher = publisher;
-    }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
+    }
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+    public Book(String title, String isbn, Publisher publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
     }
 
     public long getId() {
@@ -65,11 +74,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
